0fb5f1c59c991aeaba763db8abfe80d12f245e3a
shirohasuki <whmio0115@gmail.com>
2025-08-23 23:16:43 +0800

    [workflow] fix: fix bugs in bbdev verilator

--- workflow/steps/verilator/05_sim_event_step.py
+++ workflow/steps/verilator/05_sim_event_step.py
@@ -36,20 +36,22 @@ async def handler(data, context):
   
   # 创建带时间戳和binary名字的日志目录
   log_dir = f"{arch_dir}/log/{timestamp}-{binary_name}"
+  waveform_dir = f"{arch_dir}/waveform"
+  waveform_fst_dir = f"{arch_dir}/waveform/{timestamp}-{binary_name}"
   topname = "TestHarness"
-  
+
   os.makedirs(log_dir, exist_ok=True)
+  os.makedirs(waveform_dir, exist_ok=True)
   
   bin_path = f"{build_dir}/obj_dir/V{topname}"
-  
-  args = ""
-  if binary:
-    args = f"+permissive +loadmem={binary} +loadmem_addr=80000000 +permissive-off {binary} "
-  
-  # redirect = f"2> >(spike-dasm > {log_dir}/disasm.log)"
 
-  # subprocess.run(f"{bin_path} {args}", shell=True, check=True, text=True)
-  # subprocess.run(f"{bin_path} {args}", check=True, text=True)
-  subprocess.run(f"source {bbdir}/env.sh && ./scripts/sim.sh {bin_path} {binary} {log_dir}/stdout.log {log_dir}/disasm.log", cwd=os.path.dirname(__file__), shell=True, check=True, text=True)
+  batch = data.get("batch", False)
+  
+  subprocess.run(f"rm {waveform_dir}/waveform.vcd {waveform_dir}/waveform.fst ", shell=True, check=True)
+  subprocess.run(f"./scripts/sim.sh {bin_path} {binary} {log_dir}/stdout.log {log_dir}/disasm.log {batch}", cwd=os.path.dirname(__file__), shell=True, check=True, text=True)
+  
+  # if assert failed, vcd2fst will not be executed, and waveform.fst will not be generated
+  os.makedirs(waveform_fst_dir, exist_ok=True)
+  subprocess.run(f"vcd2fst -v {waveform_dir}/waveform.vcd -f {waveform_fst_dir}/waveform.fst", cwd=arch_dir, shell=True, check=True, text=True)
 
   await context.emit({"topic": "verilator.complete", "data": {**data, "task": "sim"}})
