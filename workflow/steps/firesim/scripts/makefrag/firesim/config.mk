aab372c6fe55442b60e64d4e57396b02c43e42a7
shirohasuki <whmio0115@gmail.com>
2025-09-05 22:34:39 +0800

    [bbdev/firesim] feat: update supports for FireSimBuckyballToyConfig on firesim

---
+++ workflow/steps/firesim/scripts/makefrag/firesim/config.mk
@@ -0,0 +1,30 @@
+# Custom configuration for BuckyBall FireSim builds
+# This file overrides the default TARGET_CONFIG_PACKAGE to use our custom configs
+
+# Only used in this projects makefrags
+makefile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
+makefile_dir := $(patsubst %/,%,$(dir $(makefile_path)))
+chipyard_dir := $(abspath $(makefile_dir)/../../../../../../arch/thirdparty/chipyard)
+
+# These point at the main class of the target's Chisel generator
+DESIGN_PACKAGE ?= firechip.chip
+DESIGN ?= FireSim
+
+# Override to use our custom config package  
+TARGET_CONFIG_PACKAGE ?= sims.firesim
+TARGET_CONFIG ?= FireSimBuckyballToyConfig
+
+# These guide chisel elaboration of simulation components by MIDAS,
+# including models and widgets.
+PLATFORM_CONFIG_PACKAGE ?= firesim.firesim
+PLATFORM_CONFIG ?= BaseF1Config
+
+# Override project for the target.
+TARGET_SBT_PROJECT := buckyball
+
+# Point to our project directory
+TARGET_SBT_DIR := $(abspath $(makefile_dir)/../../../../../../arch)
+TARGET_SOURCE_DIRS := $(abspath $(makefile_dir)/../../../../../../arch/src/main/scala)
+
+# SBT launcher
+SBT ?= java -jar $(chipyard_dir)/scripts/sbt-launch.jar $(SBT_OPTS)
