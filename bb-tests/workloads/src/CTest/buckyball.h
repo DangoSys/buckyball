333607cc9feeb298c6809d80624c8e9fd2a1c0cc
shirohasuki <whmio0115@gmail.com>
2025-09-14 22:11:21 +0800

    [bb-test] refactor: maintain customext and workloads in one CMake [bb-test/customext] refactor: reuse the bbhw abstract [bb-test/lib/bbhw] feat: add bb_insts into  the bbhw abstract

--- bb-tests/workloads/src/CTest/buckyball.h
+++ bb-tests/workloads/src/CTest/buckyball.h
@@ -4,144 +4,17 @@
 #include <stdint.h>
 
 // String macros (from xcustom.h)
-#define STR1(x) #x
-#ifndef STR
-#define STR(x) STR1(x)
-#endif
+// #define STR1(x) #x
+// #ifndef STR
+// #define STR(x) STR1(x)
+// #endif
 
 #define CAT_(A, B) A##B
 #define CAT(A, B) CAT_(A, B)
 
-// Buckyball accelerator configuration
-// #define DIM 16                    // Matrix dimension
-#define SPAD_ADDR_LEN 14 // Scratchpad address length in bits
-#define MEM_ADDR_LEN 32  // Memory address length in bits
-// #define BANK_NUM 4               // Number of banks
-// #define BANK_ROWS 4096           // Rows per bank
-// #define SP_MATRICES ((BANK_NUM * BANK_ROWS) / DIM)  // Number of matrices in
-// scratchpad
-
-// RoCC instruction encoding
-#define CUSTOM_3 0x7B // Custom-3 opcode (0111 1011)
-
-// Buckyball function codes (funct7 field)
-#define BB_MVIN_FUNCT 24      // 0x18 - Move in function code
-#define BB_MVOUT_FUNCT 25     // 0x19 - Move out function code
-#define BB_FENCE_FUNCT 31     // 0x1F - Fence function code
-#define BB_MUL_FUNCT 32       // 0x20 - Matrix multiply function code
-#define BB_IM2COL_FUNCT 33    // 0x21 - Matrix im2col function code
-#define BB_TRANSPOSE_FUNCT 34 // 0x22 - Matrix transpose function code
-#define BB_FLUSH_FUNCT 7      // 0x07 - Flush function code
-#define BB_BBFP_MUL_FUNCT 26  // 0x1A - BBFP matrix multiply function code
-#define BB_MATMUL_WS_FUNCT                                                     \
-  27 // 0x1B - Matrix multiply with warp16 function code
 // Data type for matrix elements
 typedef int8_t elem_t;
 typedef int32_t result_t;
-
-// Buckyball RoCC instruction macro (following xcustom.h style)
-#define BUCKYBALL_INSTRUCTION_R_R(rs1, rs2, func7)                             \
-  {                                                                            \
-    asm volatile(                                                              \
-        ".insn r " STR(CUSTOM_3) ", " STR(0x3) ", " STR(func7) ", x0, %0, %1"  \
-        :                                                                      \
-        : "r"(rs1), "r"(rs2));                                                 \
-  }
-
-#define BUCKYBALL_INSTRUCTION_FLUSH(func7)                                     \
-  {                                                                            \
-    asm volatile(                                                              \
-        ".insn r " STR(CUSTOM_3) ", " STR(0x3) ", " STR(func7) ", x0, x0, x0"  \
-        :                                                                      \
-        :);                                                                    \
-  }
-
-// Buckyball custom instructions using inline assembly
-
-// Move data from DRAM to scratchpad
-// mem_addr: DRAM address, sp_addr: scratchpad address, iter: number of times to
-// transfer
-#define bb_mvin(mem_addr, sp_addr, iter)                                       \
-  do {                                                                         \
-    uint64_t rs1_val = (uint64_t)(mem_addr);                                   \
-    uint64_t rs2_val = ((iter) << SPAD_ADDR_LEN) |                             \
-                       ((sp_addr) & ((1UL << SPAD_ADDR_LEN) - 1));             \
-    BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_MVIN_FUNCT);                \
-  } while (0)
-
-// Move data from scratchpad to DRAM
-// mem_addr: DRAM address, sp_addr: scratchpad address, iter: number of times to
-// transfer
-#define bb_mvout(mem_addr, sp_addr, iter)                                      \
-  do {                                                                         \
-    uint64_t rs1_val = (uint64_t)(mem_addr);                                   \
-    uint64_t rs2_val = ((iter) << SPAD_ADDR_LEN) |                             \
-                       ((sp_addr) & ((1UL << SPAD_ADDR_LEN) - 1));             \
-    BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_MVOUT_FUNCT);               \
-  } while (0)
-#define bb_fence()                                                             \
-  do {                                                                         \
-    uint64_t rs1_val = 0;                                                      \
-    uint64_t rs2_val = 0;                                                      \
-    BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_FENCE_FUNCT);               \
-  } while (0)
-// Matrix multiplication with warp16 pattern
-// op1_addr: first operand scratchpad address, op2_addr: second operand
-// scratchpad address wr_addr: write result scratchpad address, iter: number of
-// iterations
-#define bb_mul_warp16(op1_addr, op2_addr, wr_addr, iter)                       \
-  do {                                                                         \
-    uint64_t rs1_val = ((op2_addr) << SPAD_ADDR_LEN) |                         \
-                       ((op1_addr) & ((1UL << SPAD_ADDR_LEN) - 1));            \
-    uint64_t rs2_val = ((iter) << SPAD_ADDR_LEN) |                             \
-                       ((wr_addr) & ((1UL << SPAD_ADDR_LEN) - 1));             \
-    BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_MUL_FUNCT);                 \
-  } while (0)
-
-#define bb_bbfp_mul(op1_addr, op2_addr, wr_addr, iter)                         \
-  do {                                                                         \
-    uint64_t rs1_val = ((op2_addr) << SPAD_ADDR_LEN) |                         \
-                       ((op1_addr) & ((1UL << SPAD_ADDR_LEN) - 1));            \
-    uint64_t rs2_val = ((iter) << SPAD_ADDR_LEN) |                             \
-                       ((wr_addr) & ((1UL << SPAD_ADDR_LEN) - 1));             \
-    BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_BBFP_MUL_FUNCT);            \
-  } while (0)
-
-// OP1 Psum地址 OP2新的激活地址 wraddr写回的psum地址 iter迭代次数
-#define bb_matmul_ws(op1_addr, op2_addr, wr_addr, iter)                        \
-  do {                                                                         \
-    uint64_t rs1_val = ((op2_addr) << SPAD_ADDR_LEN) |                         \
-                       ((op1_addr) & ((1UL << SPAD_ADDR_LEN) - 1));            \
-    uint64_t rs2_val =                                                         \
-        (((1) << (SPAD_ADDR_LEN + 10)) | (iter) << SPAD_ADDR_LEN) |            \
-        ((wr_addr) & ((1UL << SPAD_ADDR_LEN) - 1));                            \
-    BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_MATMUL_WS_FUNCT);           \
-  } while (0)
-
-#define bb_im2col(op1_addr, wr_addr, krow, kcol, inrow, incol, startrow,       \
-                  startcol)                                                    \
-  do {                                                                         \
-    uint64_t rs1_val = ((wr_addr) << SPAD_ADDR_LEN) |                          \
-                       ((op1_addr) & ((1UL << SPAD_ADDR_LEN) - 1));            \
-    uint64_t rs2_val =                                                         \
-        (((startrow) << (SPAD_ADDR_LEN + 38)) |                                \
-         ((startcol) << (SPAD_ADDR_LEN + 33)) |                                \
-         ((inrow) << (SPAD_ADDR_LEN + 23)) |                                   \
-         ((incol) << (SPAD_ADDR_LEN + 18)) |                                   \
-         ((krow) << (SPAD_ADDR_LEN + 14)) | ((kcol) << (SPAD_ADDR_LEN + 10))); \
-    BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_IM2COL_FUNCT);              \
-  } while (0)
-
-#define bb_transpose(op1_addr, wr_addr, iter)                                  \
-  do {                                                                         \
-    uint64_t rs1_val = ((wr_addr) << SPAD_ADDR_LEN) |                          \
-                       ((op1_addr) & ((1UL << SPAD_ADDR_LEN) - 1));            \
-    uint64_t rs2_val = ((iter) << SPAD_ADDR_LEN);                              \
-    BUCKYBALL_INSTRUCTION_R_R(rs1_val, rs2_val, BB_TRANSPOSE_FUNCT);           \
-  } while (0)
-
-// Flush accelerator
-#define bb_flush() BUCKYBALL_INSTRUCTION_FLUSH(BB_FLUSH_FUNCT)
 
 #define MULTICORE_INIT(hart_id)                                                \
   __attribute__((constructor)) static void _multicore_init() {                 \
