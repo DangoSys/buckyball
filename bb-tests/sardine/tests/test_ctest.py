1b1055708690d2d4c113f94a0ef726f251ae218c
shirohasuki <whmio0115@gmail.com>
2025-10-09 02:01:03 +0800

    [ci] fix: remove outdated test in readme and ci test

--- bb-tests/sardine/tests/test_ctest.py
+++ bb-tests/sardine/tests/test_ctest.py
@@ -12,14 +12,14 @@ ctest_workload_dir = sardine_dir / ".." / "output" / "workloads" / "src" / "CTes
 
 # Define all ctest workloads with absolute paths and corresponding IDs
 ctest_workloads = [
-    (
-        "ctest_mvin_mvout_acc_test_singlecore-baremetal",
-        "ctest_mvin_mvout_acc_test_singlecore-baremetal",
-    ),
-    (
-        "ctest_mvin_mvout_alternate_test_singlecore-baremetal",
-        "ctest_mvin_mvout_alternate_test_singlecore-baremetal",
-    ),
+    # (
+    #     "ctest_mvin_mvout_acc_test_singlecore-baremetal",
+    #     "ctest_mvin_mvout_acc_test_singlecore-baremetal",
+    # ),
+    # (
+    #     "ctest_mvin_mvout_alternate_test_singlecore-baremetal",
+    #     "ctest_mvin_mvout_alternate_test_singlecore-baremetal",
+    # ),
     (
         "ctest_vecunit_matmul_16xn_ones_singlecore-baremetal",
         "ctest_vecunit_matmul_16xn_ones_singlecore-baremetal",
