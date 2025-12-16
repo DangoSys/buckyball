6644183e9a4e62c77025622d06c7e3dbf2261b78
Bohan Wang <140929282+Mikemy666@users.noreply.github.com>
2025-12-16 17:15:42 +0800

    change the framework of memdomain for the preparation of virtualization (#18)

--- arch/src/main/scala/framework/memdomain/MemController.scala
+++ arch/src/main/scala/framework/memdomain/MemController.scala
@@ -4,42 +4,133 @@ import chisel3._
 import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
 import examples.BuckyballConfigs.CustomBuckyballConfig
+import framework.frontend.decoder.PostGDCmd
+import freechips.rocketchip.tile._
+import framework.memdomain.dma.{BBReadRequest, BBReadResponse, BBWriteRequest, BBWriteResponse}
 import framework.memdomain.mem.{SramReadIO, SramWriteIO, Scratchpad}
+import framework.memdomain.{MemLoader, MemStorer, MemController}
+import framework.memdomain.rs.MemReservationStation
+import framework.memdomain.tlb.{BBTLBCluster, BBTLBIO, BBTLBExceptionIO}
+import framework.memdomain.pmc.MemCyclePMC
+import freechips.rocketchip.tilelink.TLEdgeOut
+import freechips.rocketchip.rocket.TLBPTWIO
+import framework.frontend.globalrs.{GlobalRsIssue, GlobalRsComplete}
+import framework.balldomain.blink.{SramReadWithInfo, SramWriteWithInfo}
+import framework.switcher.{ToPhysicalLine, ToVirtualLine}
 
 /**
  * MemController: Controller that encapsulates scratchpad and accumulator
  * Provides DMA interface and Ball Domain interface
  */
-class MemController(implicit b: CustomBuckyballConfig, p: Parameters) extends Module {
+class MemController(implicit b: CustomBuckyballConfig, p: Parameters, edge: TLEdgeOut) extends Module {
+  private val numBanks = b.sp_banks + b.acc_banks
   val io = IO(new Bundle {
-    // DMA interface - for MemLoader and MemStorer access
-    val dma = new Bundle {
-      val sramRead  = Vec(b.sp_banks, new SramReadIO(b.spad_bank_entries, b.spad_w))
-      val sramWrite = Vec(b.sp_banks, new SramWriteIO(b.spad_bank_entries, b.spad_w, b.spad_mask_len))
-      val accRead   = Vec(b.acc_banks, new SramReadIO(b.acc_bank_entries, b.acc_w))
-      val accWrite  = Vec(b.acc_banks, new SramWriteIO(b.acc_bank_entries, b.acc_w, b.acc_mask_len))
+    // Issue interface from global RS (single channel)
+    val global_issue_i = Flipped(Decoupled(new GlobalRsIssue))
+
+    // Report completion to global RS (single channel)
+    val global_complete_o = Decoupled(new GlobalRsComplete)
+
+    // SRAM read/write interface - used by load/store
+    val interdma = new Bundle {
+      val sramread  = Vec(b.sp_banks, Flipped(new SramReadIO(b.spad_bank_entries, b.spad_w)))
+      val sramwrite = Vec(b.sp_banks, Flipped(new SramWriteIO(b.spad_bank_entries, b.spad_w, b.spad_mask_len)))
+      val accread   = Vec(b.acc_banks, Flipped(new SramReadIO(b.acc_bank_entries, b.acc_w)))
+      val accwrite  = Vec(b.acc_banks, Flipped(new SramWriteIO(b.acc_bank_entries, b.acc_w, b.acc_mask_len)))
     }
 
-    // Ball Domain interface - for BallController access
-    val ballDomain = new Bundle {
-      val sramRead  = Vec(b.sp_banks, new SramReadIO(b.spad_bank_entries, b.spad_w))
-      val sramWrite = Vec(b.sp_banks, new SramWriteIO(b.spad_bank_entries, b.spad_w, b.spad_mask_len))
-      val accRead   = Vec(b.acc_banks, new SramReadIO(b.acc_bank_entries, b.acc_w))
-      val accWrite  = Vec(b.acc_banks, new SramWriteIO(b.acc_bank_entries, b.acc_w, b.acc_mask_len))
+    // DMA interface - used by Outer DRAM controller
+    val intradma = new Bundle {
+      val read = new Bundle {
+        val req = Decoupled(new BBReadRequest())
+        val resp = Flipped(Decoupled(new BBReadResponse(b.spad_w)))
+      }
+      val write = new Bundle {
+        val req = Decoupled(new BBWriteRequest(b.spad_w))
+        val resp = Flipped(Decoupled(new BBWriteResponse))
+      }
     }
+
+    // TLB interface - exposed externally for DMA use
+    val tlb = Vec(2, Flipped(new BBTLBIO))
+
+    // PTW interface - needs to connect to upper level PTW (shared TLB has only 1 PTW)
+    val ptw = Vec(1, new TLBPTWIO)
+
+    // TLB exception interface - exposed to upper level for handling flush, etc. (shared TLB has only 1 exp)
+    val tlbExp = Vec(1, new BBTLBExceptionIO)
+
+    // Busy signal
+    val busy = Output(Bool())
   })
 
-  val spad = Module(new Scratchpad(b))
+  val memDecoder = Module(new MemDomainDecoder)
+  val memRs      = Module(new MemReservationStation)
+  val memLoader  = Module(new MemLoader)
+  val memStorer  = Module(new MemStorer)
+  // TLB cluster - use shared TLB like Gemmini
+  val tlbCluster = Module(new BBTLBCluster(2, b.tlb_size, b.dma_maxbytes, use_shared_tlb = true))
 
-  // Connect DMA interface to Scratchpad's DMA ports
-  io.dma.sramRead   <> spad.io.dma.sramread
-  io.dma.sramWrite  <> spad.io.dma.sramwrite
-  io.dma.accRead    <> spad.io.dma.accread
-  io.dma.accWrite   <> spad.io.dma.accwrite
+// -----------------------------------------------------------------------------
+// Global RS -> MemDecoder
+// -----------------------------------------------------------------------------
+  memDecoder.io.raw_cmd_i.valid := io.global_issue_i.valid
+  memDecoder.io.raw_cmd_i.bits  := io.global_issue_i.bits.cmd
+  io.global_issue_i.ready := memDecoder.io.raw_cmd_i.ready
 
-  // Connect Ball Domain interface to Scratchpad's execution ports
-  io.ballDomain.sramRead  <> spad.io.exec.sramread
-  io.ballDomain.sramWrite <> spad.io.exec.sramwrite
-  io.ballDomain.accRead   <> spad.io.exec.accread
-  io.ballDomain.accWrite  <> spad.io.exec.accwrite
+// -----------------------------------------------------------------------------
+// MemDecoder -> MemReservationStation
+// -----------------------------------------------------------------------------
+  // Connect decoded instruction and global rob_id
+  memRs.io.mem_decode_cmd_i.valid := memDecoder.io.mem_decode_cmd_o.valid
+  memRs.io.mem_decode_cmd_i.bits.cmd := memDecoder.io.mem_decode_cmd_o.bits
+  memRs.io.mem_decode_cmd_i.bits.rob_id := io.global_issue_i.bits.rob_id
+  memDecoder.io.mem_decode_cmd_o.ready := memRs.io.mem_decode_cmd_i.ready
+
+// -----------------------------------------------------------------------------
+// MemReservationStation -> MemLoader/MemStorer
+// -----------------------------------------------------------------------------
+  memLoader.io.cmdReq <> memRs.io.issue_o.ld
+  memStorer.io.cmdReq <> memRs.io.issue_o.st
+  memRs.io.commit_i.ld <> memLoader.io.cmdResp
+  memRs.io.commit_i.st <> memStorer.io.cmdResp
+
+//-----------------------------------------------------------------------------
+// PMC - Performance Monitor Counter
+// -----------------------------------------------------------------------------
+  val pmc = Module(new MemCyclePMC)
+  pmc.io.ldReq_i.valid := memRs.io.issue_o.ld.fire
+  pmc.io.ldReq_i.bits := memRs.io.issue_o.ld.bits
+  pmc.io.stReq_i.valid := memRs.io.issue_o.st.fire
+  pmc.io.stReq_i.bits := memRs.io.issue_o.st.bits
+  pmc.io.ldResp_o.valid := memLoader.io.cmdResp.fire
+  pmc.io.ldResp_o.bits := memLoader.io.cmdResp.bits
+  pmc.io.stResp_o.valid := memStorer.io.cmdResp.fire
+  pmc.io.stResp_o.bits := memStorer.io.cmdResp.bits
+// 
+  // Connect MemLoader and MemStorer to DMA
+  memLoader.io.dmaReq <> io.intradma.read.req
+  io.intradma.read.resp <> memLoader.io.dmaResp
+  memStorer.io.dmaReq <> io.intradma.write.req
+  io.intradma.write.resp <> memStorer.io.dmaResp
+
+  // Connect TLB - now using internal BBTLBCluster
+  io.tlb <> tlbCluster.io.clients
+  io.ptw <> tlbCluster.io.ptw
+
+  // Connect exception interface - note direction: internal TLB's exp is Output, external interface is Input
+  tlbCluster.io.exp <> io.tlbExp
+
+  // Connect MemLoader and MemStorer to MemController's DMA interface
+  memLoader.io.sramWrite <> io.interdma.sramwrite
+  memLoader.io.accWrite <>io.interdma.accwrite
+  memStorer.io.sramRead <> io.interdma.sramread
+  memStorer.io.accRead <> io.interdma.accread
+
+  // Completion signal connected to global RS
+  io.global_complete_o <> memRs.io.complete_o
+
+  // Busy signal
+  // Simple busy signal
+  io.busy := !memRs.io.complete_o.ready
 }
