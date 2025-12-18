d86434b67ba2d0579b92061e007a04cf52e5c359
Mikemy666 <2535634982@qq.com>
2025-12-18 12:59:40 +0800

    [arch/memdomain] refactor: made some adjustments to the architecture details to prepare for backend virtualization.

--- arch/src/main/scala/framework/memdomain/Translator.scala
+++ arch/src/main/scala/framework/memdomain/Translator.scala
@@ -5,13 +5,13 @@ import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
 import examples.BuckyballConfigs.CustomBuckyballConfig
 import framework.memdomain.mem.{SramReadIO, SramWriteIO}
+import framework.balldomain.blink.{SramReadWithInfo, SramWriteWithInfo}
 
 class Translator(implicit b: CustomBuckyballConfig, p: Parameters) extends Module{
+  val numBanks = b.sp_banks + b.acc_banks
   class BanksIO extends Bundle {
-    val sramread  = Vec(b.sp_banks, new SramReadIO(b.spad_bank_entries, b.spad_w))
-    val sramwrite = Vec(b.sp_banks, new SramWriteIO(b.spad_bank_entries, b.spad_w, b.spad_mask_len))
-    val accread   = Vec(b.acc_banks, new SramReadIO(b.acc_bank_entries, b.acc_w))
-    val accwrite  = Vec(b.acc_banks, new SramWriteIO(b.acc_bank_entries, b.acc_w, b.acc_mask_len))
+    val sramread  = Vec(numBanks, new SramReadWithInfo(b.spad_bank_entries, b.spad_w))
+    val sramwrite = Vec(numBanks, new SramWriteWithInfo(b.spad_bank_entries, b.spad_w, b.spad_mask_len))
   }
 
   val io = IO(new Bundle {
