e50ee903dfdfaba0967e9fab3e10d860e235c23f
SJM946 <coultrapzahnop442@gmail.com>
2026-01-04 19:04:59 +0800

    Fix some bugs

---
+++ arch/src/main/scala/palladium/fpga/package.scala
@@ -0,0 +1,16 @@
+package object palladium {
+  object fpga {
+    import org.chipsalliance.cde.config.{Config, Field}
+
+    case object FPGAFrequencyKey extends Field[Double](100.0) // Default to 100MHz
+    case object VCU19PTweaksKey extends Field[Boolean](true)
+
+    class WithFPGAFrequency(freqMHz: Double) extends Config((site, here, up) => {
+      case FPGAFrequencyKey => freqMHz
+    })
+
+    class WithVCU19PTweaks extends Config((site, here, up) => {
+      case VCU19PTweaksKey => true
+    })
+  }
+}
