f37d808c98fa3352c23224ae39e05fb921a9195e
shirohasuki <whmio0115@gmail.com>
2026-01-04 20:19:06 +0800

    [arch] feat: introduce ChannelMappingTable

--- arch/src/main/scala/framework/balldomain/bbus/memrouter/memRouter.scala
+++ arch/src/main/scala/framework/balldomain/bbus/memrouter/memRouter.scala
@@ -8,10 +8,14 @@ import framework.top.GlobalConfig
 
 class PeakChannelReq(val b: GlobalConfig) extends Bundle {
   val needed_channel_num = UInt(log2Up(b.ballDomain.ballIdMappings.map(_.inBW).sum + 1).W)
+  val bank_id            = UInt(log2Up(b.memDomain.bankNum).W)
+  val rob_id             = UInt(log2Up(b.frontend.rob_entries).W)
 }
 
 class FreeChannelResp(val b: GlobalConfig) extends Bundle {
-  val is_free = Bool()
+  val is_free     = Bool()
+  val channel_ids = Vec(b.top.ballMemChannelProducer, UInt(log2Up(b.top.ballMemChannelProducer).W))
+  val channel_num = UInt(log2Up(b.top.ballMemChannelProducer + 1).W)
 }
 
 @instantiable
@@ -35,16 +39,48 @@ class MemRouter(val b: GlobalConfig) extends Module {
     val freeChannelResp = Decoupled(new FreeChannelResp(b))
   })
 
-  val readReqGen: Instance[ReadReqGen] = Instantiate(new ReadReqGen(b))
+//---------------------------------------------------------------------------
+// Read
+//---------------------------------------------------------------------------
+// Step1: Generate read request
+  val readReqGen:          Instance[ReadReqGen]          = Instantiate(new ReadReqGen(b))
+  val channelMappingTable: Instance[ChannelMappingTable] = Instantiate(new ChannelMappingTable(b))
   readReqGen.io.bank_read_i <> io.bankRead_i
-
-  val hasReadReq    = readReqGen.io.read_req_o.valid
-  val isChannelFree = io.freeChannelResp.valid && io.freeChannelResp.bits.is_free
-  val reqAccepted   = isChannelFree
-
+// Step2: Peek if there are enough free channels
+  val hasReadReq = readReqGen.io.read_req_o.valid
   io.peakChannelReq.valid                   := hasReadReq
   io.peakChannelReq.bits.needed_channel_num := readReqGen.io.read_req_o.bits.channel_num
-  io.freeChannelResp.ready                  := hasReadReq
+  io.peakChannelReq.bits.bank_id            := readReqGen.io.read_req_o.bits.bank_id
+  io.peakChannelReq.bits.rob_id             := readReqGen.io.read_req_o.bits.rob_id
+// Step3/1: if there are enough free channels, accept the request
+  val isChannelFree = io.freeChannelResp.valid && io.freeChannelResp.bits.is_free
+  io.freeChannelResp.ready := true.B
+  val dispatchChannels = io.freeChannelResp.bits.channel_ids
+  readReqGen.io.read_req_o.ready := isChannelFree
+// Step3/2: if there are not enough free channels, continue to peek until there are enough free channels
 
-  readReqGen.io.read_req_o.ready := reqAccepted
+// Step4: Set the channel mapping table
+  when(readReqGen.io.read_req_o.fire) {
+    var matchedCnt = 0.U
+    for (i <- 0 until totalReadChannels) {
+      val isMatch = io.bankRead_i(i).ball_id === readReqGen.io.read_req_o.bits.ball_id &&
+        io.bankRead_i(i).bank_id === readReqGen.io.read_req_o.bits.bank_id && io.bankRead_i(i).io.req.valid
+      when(isMatch && matchedCnt < readReqGen.io.read_req_o.bits.channel_num) {
+        channelMappingTable.io.write.valid      := true.B
+        channelMappingTable.io.write.bits.idx   := i.U
+        channelMappingTable.io.write.bits.outCh := dispatchChannels(matchedCnt)
+        matchedCnt = matchedCnt + 1.U
+      }
+    }
+  }
+// Step5: Dispatch the request to the channels
+  for (i <- 0 until totalReadChannels) {
+    when(channelMappingTable.io.routeValid(i)) {
+      val outCh = channelMappingTable.io.routeMap(i)
+      io.bankRead_o(outCh).io.req <> io.bankRead_i(i).io.req
+      io.bankRead_o(outCh).ball_id := io.bankRead_i(i).ball_id
+      io.bankRead_o(outCh).bank_id := io.bankRead_i(i).bank_id
+      io.bankRead_o(outCh).rob_id  := io.bankRead_i(i).rob_id
+    }
+  }
 }
--- arch/src/main/scala/framework/top/channels/BallMemChannel.scala
+++ arch/src/main/scala/framework/top/channels/BallMemChannel.scala
@@ -25,8 +25,54 @@ class BallMemChannel(val b: GlobalConfig) extends Module {
   VecInit(channelsToMem.map(_.io)) <> io.channelToMemDomain
   VecInit(channelsFromMem.map(_.io)) <> io.channelFromMemDomain
 
-  val freeChannelCount = PopCount(io.channelToMemDomain.map(!_.header.read.lock))
-  io.freeChannelResp.valid        := io.peakChannelReq.valid
-  io.freeChannelResp.bits.is_free := freeChannelCount >= io.peakChannelReq.bits.needed_channel_num
-  io.peakChannelReq.ready         := io.freeChannelResp.ready
+  val headerBuffers = (0 until b.top.ballMemChannelProducer).map(_ => Instantiate(new HeaderBuffer(b)))
+
+  val freeChannels      = VecInit(headerBuffers.map(!_.io.read.lock))
+  val freeChannelCount  = PopCount(freeChannels)
+  val hasEnoughChannels = freeChannelCount >= io.peakChannelReq.bits.needed_channel_num
+
+  val selectedChannelIds = Wire(Vec(b.top.ballMemChannelProducer, UInt(log2Up(b.top.ballMemChannelProducer).W)))
+  val selectedMask       = Wire(Vec(b.top.ballMemChannelProducer, Bool()))
+
+  val masks = Wire(Vec(b.top.ballMemChannelProducer + 1, Vec(b.top.ballMemChannelProducer, Bool())))
+  masks(0) := freeChannels
+
+  for (i <- 0 until b.top.ballMemChannelProducer) {
+    val maskVec = masks(i)
+    val selOH   = PriorityEncoderOH(maskVec.asUInt)
+    val selIdx  = PriorityEncoder(maskVec.asUInt)
+    selectedMask(i)       := maskVec.asUInt.orR && (i.U < io.peakChannelReq.bits.needed_channel_num)
+    selectedChannelIds(i) := Mux(selectedMask(i), selIdx, 0.U)
+    if (i < b.top.ballMemChannelProducer) {
+      masks(i + 1) := VecInit(maskVec.zip(selOH.asBools).map { case (free, sel) => free && !sel })
+    }
+  }
+
+  io.freeChannelResp.valid            := io.peakChannelReq.valid
+  io.freeChannelResp.bits.is_free     := hasEnoughChannels
+  io.freeChannelResp.bits.channel_ids := selectedChannelIds
+  io.freeChannelResp.bits.channel_num := io.peakChannelReq.bits.needed_channel_num
+  io.peakChannelReq.ready             := io.freeChannelResp.ready
+
+  when(io.peakChannelReq.fire && hasEnoughChannels) {
+    for (i <- 0 until b.top.ballMemChannelProducer) {
+      val isSelected = selectedMask.zip(selectedChannelIds).map { case (sel, id) => sel && (id === i.U) }.reduce(_ || _)
+      when(isSelected) {
+        val headerData = Wire(new HeaderBufferData(b))
+        headerData.lock               := true.B
+        headerData.rob_id             := io.peakChannelReq.bits.rob_id
+        headerData.bank_id            := io.peakChannelReq.bits.bank_id
+        headerBuffers(i).io.set.valid := true.B
+        headerBuffers(i).io.set.bits  := headerData
+      }.otherwise {
+        headerBuffers(i).io.set.valid := false.B
+        headerBuffers(i).io.set.bits  := DontCare
+      }
+    }
+  }.otherwise {
+    for (i <- 0 until b.top.ballMemChannelProducer) {
+      headerBuffers(i).io.set.valid := false.B
+      headerBuffers(i).io.set.bits  := DontCare
+    }
+  }
 }
