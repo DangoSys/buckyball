49dececf99807c092f8d5d4efce2f9847b51356e
Mikemy666 <2535634982@qq.com>
2026-01-06 16:45:21 +0800

    [arch]fix:fix the bugs of toybuckyball

--- arch/src/main/scala/examples/toy/ToyBuckyBall.scala
+++ arch/src/main/scala/examples/toy/ToyBuckyBall.scala
@@ -102,7 +102,35 @@ class ToyBuckyballModule(outer: ToyBuckyball) extends LazyRoCCModuleImp(outer) w
   gpDomain.io.global_issue_i <> frontend.io.gp_issue_o
   frontend.io.gp_complete_i <> gpDomain.io.global_complete_o
 
-  ballDomain.bankRead <> memDomain.io.ballDomain.bankRead
+  // Break potential combinational loops on bankRead by registering the req channel.
+  // IDs are queued alongside the req bits to keep them aligned through backpressure.
+  for (i <- 0 until b.top.memBallChannelNum) {
+    val bankReadReqWithIds = Wire(Decoupled(new Bundle {
+      val bank_id = chiselTypeOf(ballDomain.bankRead(i).bank_id)
+      val rob_id  = chiselTypeOf(ballDomain.bankRead(i).rob_id)
+      val ball_id = chiselTypeOf(ballDomain.bankRead(i).ball_id)
+      val req     = chiselTypeOf(ballDomain.bankRead(i).io.req.bits)
+    }))
+
+    bankReadReqWithIds.valid := ballDomain.bankRead(i).io.req.valid
+    bankReadReqWithIds.bits.bank_id := ballDomain.bankRead(i).bank_id
+    bankReadReqWithIds.bits.rob_id  := ballDomain.bankRead(i).rob_id
+    bankReadReqWithIds.bits.ball_id := ballDomain.bankRead(i).ball_id
+    bankReadReqWithIds.bits.req     := ballDomain.bankRead(i).io.req.bits
+    ballDomain.bankRead(i).io.req.ready := bankReadReqWithIds.ready
+
+    val bankReadReqQ = Queue(bankReadReqWithIds, 1)
+
+    memDomain.io.ballDomain.bankRead(i).io.req.valid := bankReadReqQ.valid
+    memDomain.io.ballDomain.bankRead(i).io.req.bits  := bankReadReqQ.bits.req
+    memDomain.io.ballDomain.bankRead(i).bank_id := bankReadReqQ.bits.bank_id
+    memDomain.io.ballDomain.bankRead(i).rob_id  := bankReadReqQ.bits.rob_id
+    memDomain.io.ballDomain.bankRead(i).ball_id := bankReadReqQ.bits.ball_id
+    bankReadReqQ.ready := memDomain.io.ballDomain.bankRead(i).io.req.ready
+
+    ballDomain.bankRead(i).io.resp <> memDomain.io.ballDomain.bankRead(i).io.resp
+  }
+
   ballDomain.bankWrite <> memDomain.io.ballDomain.bankWrite
 
   // Connect BallMemChannelCluster
