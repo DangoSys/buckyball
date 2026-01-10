aea83c10861c18cc3e3a60545d1e09d4003edf5b
shirohasuki <whmio0115@gmail.com>
2026-01-10 23:01:23 +0800

    [bebop] feat: bump bebop to the latest version.
    (testing workloads waits vmem branch to be merged)

--- bebop/bebop/src/simulator/mod.rs
+++
@@ -1,7 +0,0 @@
-/// Simulator module
-#[macro_use]
-pub mod utils;
-pub mod server;
-pub mod simulator;
-
-pub use simulator::{Simulator, StepMode};
--- bebop/bebop/src/simulator/server/socket/mod.rs
+++
@@ -1,8 +0,0 @@
-/// Socket communication module for host-Bebop interface
-pub mod cmd;
-pub mod protocol;
-pub mod read;
-pub mod write;
-
-pub use cmd::{CmdReq, CmdResp, SocketMsg, SocketResp};
-pub use protocol::MsgType;
