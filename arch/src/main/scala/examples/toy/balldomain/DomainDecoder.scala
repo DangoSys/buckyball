20bf9daae17683cb66c1c353c97c82e1e21e3965
Mikemy666 <2535634982@qq.com>
2026-02-11 17:33:17 +0800

    [arch]fix:fix some bugs of transposeball

--- arch/src/main/scala/examples/toy/balldomain/DomainDecoder.scala
+++ arch/src/main/scala/examples/toy/balldomain/DomainDecoder.scala
@@ -73,7 +73,9 @@ class BallDomainDecoder(val b: GlobalConfig) extends Module {
     Array(
       MATMUL_WARP16 -> List(Y, Y, Y, Y, Y, rs1(7, 0), rs2(7, 0), rs1(15, 8), rs2(15, 8), 0.U, rs2(63, 16)),
       RELU          -> List(Y, N, Y, Y, N, rs1(7, 0), DADDR, rs2(7, 0), rs2(15, 8), 1.U, rs2(63, 16)),
-      TRANSPOSE     -> List(Y, Y, Y, Y, Y, rs1(7, 0), rs2(7, 0), rs1(15, 8), rs2(15, 8), 2.U, rs2(63, 16)),
+      // Transpose only reads op1 and writes wr_bank; it does NOT consume op2.
+      // Enabling op2 here can stall/abort when op2_bank defaults to op1_bank.
+      TRANSPOSE     -> List(Y, N, Y, Y, N, rs1(7, 0), DADDR, rs1(15, 8), rs2(15, 8), 2.U, rs2(63, 16)),
       IM2COL        -> List(Y, Y, Y, Y, Y, rs1(7, 0), rs2(7, 0), rs1(15, 8), rs2(15, 8), 3.U, rs2(63, 16)),
       CONCAT        -> List(Y, N, Y, Y, N, rs1(7, 0), DADDR, rs2(7, 0), rs2(15, 8), 5.U, rs2(63, 16)),
       TRANSFER      -> List(Y, N, Y, Y, N, rs1(7, 0), DADDR, rs2(7, 0), rs2(15, 8), 6.U, rs2(63, 16))
