1be07a3757024e5984e64c224324eabef6a51682
Mikemy666 <2535634982@qq.com>
2026-03-05 15:03:53 +0800

    [arch]add:build shared mem framework

---
+++ arch/src/main/scala/framework/memdomain/backend/shared/SharedMem.scala
@@ -0,0 +1,95 @@
+package framework.memdomain.backend.shared
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.hierarchy.{instantiable, public}
+import framework.top.GlobalConfig
+import framework.memdomain.backend.banks.{SramReadResp, SramWriteResp}
+
+class SharedMemReadReq(val b: GlobalConfig) extends Bundle {
+	val vbank_id = UInt(log2Up(b.memDomain.bankNum).W)
+	val group_id = UInt(3.W)
+	val addr     = UInt(log2Ceil(b.memDomain.bankEntries).W)
+}
+
+class SharedMemWriteReq(val b: GlobalConfig) extends Bundle {
+	val vbank_id = UInt(log2Up(b.memDomain.bankNum).W)
+	val group_id = UInt(3.W)
+	val addr     = UInt(log2Ceil(b.memDomain.bankEntries).W)
+	val mask     = Vec(b.memDomain.bankMaskLen, Bool())
+	val data     = UInt(b.memDomain.bankWidth.W)
+	val wmode    = Bool()
+}
+
+class SharedMemReadIO(val b: GlobalConfig) extends Bundle {
+	val req  = Flipped(Decoupled(new SharedMemReadReq(b)))
+	val resp = Decoupled(new SramReadResp(b))
+}
+
+class SharedMemWriteIO(val b: GlobalConfig) extends Bundle {
+	val req  = Flipped(Decoupled(new SharedMemWriteReq(b)))
+	val resp = Decoupled(new SramWriteResp(b))
+}
+
+@instantiable
+class SharedMem(val b: GlobalConfig) extends Module {
+	private val maxGroup       = 8
+	private val mask_len        = b.memDomain.bankMaskLen
+	private val mask_elem       = UInt((b.memDomain.bankWidth / mask_len).W)
+	private val minSharedLines = b.memDomain.bankNum * maxGroup * b.memDomain.bankEntries
+
+	require(
+		b.memDomain.sharedEntries >= minSharedLines,
+		s"sharedEntries=${b.memDomain.sharedEntries} is too small, minimum=$minSharedLines"
+	)
+
+	@public
+	val io = IO(new Bundle {
+		val read  = new SharedMemReadIO(b)
+		val write = new SharedMemWriteIO(b)
+	})
+
+	val mem = SyncReadMem(b.memDomain.sharedEntries, Vec(mask_len, mask_elem))
+
+	// Shared memory address mapping:
+	// shared_addr = ((vbank_id * MAX_GROUP + group_id) * bankEntries) + local_addr
+	private def toSharedAddr(vbank_id: UInt, group_id: UInt, local_addr: UInt): UInt = {
+		val vbankPart = vbank_id * (maxGroup * b.memDomain.bankEntries).U
+		val groupPart = group_id * b.memDomain.bankEntries.U
+		vbankPart + groupPart + local_addr
+	}
+
+	io.read.req.ready  := !io.write.req.valid
+	io.write.req.ready := !io.read.req.valid
+
+	val readReqFire = io.read.req.fire
+	val readAddr    = toSharedAddr(io.read.req.bits.vbank_id, io.read.req.bits.group_id, io.read.req.bits.addr)
+
+	when(readReqFire) {
+		assert(io.read.req.bits.vbank_id < b.memDomain.bankNum.U, "SharedMem: vbank_id out of range")
+		assert(io.read.req.bits.group_id < maxGroup.U, "SharedMem: group_id out of range")
+		assert(io.read.req.bits.addr < b.memDomain.bankEntries.U, "SharedMem: local addr out of range")
+	}
+
+	val readData = mem.read(readAddr, readReqFire)
+
+	io.read.resp.valid     := RegNext(readReqFire, init = false.B)
+	io.read.resp.bits.data := readData.asUInt
+
+	val writeReqFire = io.write.req.fire
+	val writeAddr    = toSharedAddr(io.write.req.bits.vbank_id, io.write.req.bits.group_id, io.write.req.bits.addr)
+
+	when(writeReqFire) {
+		assert(io.write.req.bits.vbank_id < b.memDomain.bankNum.U, "SharedMem: vbank_id out of range")
+		assert(io.write.req.bits.group_id < maxGroup.U, "SharedMem: group_id out of range")
+		assert(io.write.req.bits.addr < b.memDomain.bankEntries.U, "SharedMem: local addr out of range")
+		mem.write(
+			writeAddr,
+			io.write.req.bits.data.asTypeOf(Vec(mask_len, mask_elem)),
+			io.write.req.bits.mask
+		)
+	}
+
+	io.write.resp.valid   := RegNext(writeReqFire, init = false.B)
+	io.write.resp.bits.ok := RegNext(writeReqFire, init = false.B)
+}
