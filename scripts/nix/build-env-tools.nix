9f533ecfb83ee726a717a4fedf77def4bc0608c1
shirohasuki <whmio0115@gmail.com>
2026-03-06 15:15:26 +0800

    [nix] update: downgrade Verilator version to 5.022

--- scripts/nix/build-env-tools.nix
+++ scripts/nix/build-env-tools.nix
@@ -24,14 +24,14 @@ let
   };
 in
 {
-  # Pin to Verilator 5.036 2025-01-01 (nixpkgs-unstable ships 5.044)
+  # Pin to Verilator 5.022 2024-02-24 (nixpkgs-unstable ships 5.044)
   verilator = pkgs.verilator.overrideAttrs (old: {
-    version = "5.036";
+    version = "5.022";
     src = pkgs.fetchurl {
-      url = "https://github.com/verilator/verilator/archive/refs/tags/v5.036.tar.gz";
-      hash = "sha256-QZmWSILVbPahnOgMail+vjsMNeqBEGzU9yI0JZQzfEc=";
+      url = "https://github.com/verilator/verilator/archive/refs/tags/v5.022.tar.gz";
+      hash = "sha256-PC9TOPS2zn4vR6FCQBrN0Yy/TF2gYJJhjW0DbAr+8S0=";
     };
-    sourceRoot = "verilator-5.036";
+    sourceRoot = "verilator-5.022";
     doCheck = false;
   });
 
