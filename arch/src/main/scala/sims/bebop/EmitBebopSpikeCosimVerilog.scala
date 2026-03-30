24b9bcafeb620b6d06f8bbbd64067c6dcc7aa309
daiyongyuan <1533208939@qq.com>
2026-03-30 14:35:44 +0800

    feat(bebop): Add Spike and Verilator cosimulation module

---
+++ arch/src/main/scala/sims/bebop/EmitBebopSpikeCosimVerilog.scala
@@ -0,0 +1,16 @@
+package sims.bebop
+
+import _root_.circt.stage.ChiselStage
+
+/** `mill buckyball.runMain sims.bebop.EmitBebopSpikeCosimVerilog <abs-or-rel-dir>` */
+object EmitBebopSpikeCosimVerilog {
+  def main(args: Array[String]): Unit = {
+    val dir = if (args.nonEmpty) args(0) else "gen-bebop-cosim"
+    ChiselStage.emitSystemVerilogFile(
+      new BebopSpikeCosimTop,
+      firtoolOpts = Array.empty,
+      args = Array("-td", dir),
+    )
+    println(s"EmitBebopSpikeCosimVerilog: wrote under $dir")
+  }
+}
