84fd6e561ec775824a0f1c3a511ff87a4f33c81d
daiyongyuan <1533208939@qq.com>
2026-08-14 16:52:29 +0800

    fix: align Verilator difftest chip topology (#90)

--- examples/cores/pebble/emu/src/lib.rs
+++ examples/cores/pebble/emu/src/lib.rs
@@ -1,4 +1,4 @@
-pub const BEMU_TOP_CONFIG: &str = "../../core.toml";
+pub const BEMU_TOP_CONFIG: &str = "../../../../chips/pebble/configs/pebble.toml";
 
 pub fn bemu_top_config() -> &'static str {
     BEMU_TOP_CONFIG
