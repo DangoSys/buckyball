a103a27d403ff3247f858892484535eacd4c3354
daiyongyuan <1533208939@qq.com>
2026-08-30 12:39:10 +0800

    fix(rushb): derive ABI accelerator IDs from Chip.pb

--- compiler/lib/RushBRuntime.c
+++ compiler/lib/RushBRuntime.c
@@ -1,7 +1,17 @@
 #include <buckyball/rushb.h>
 
+#ifndef BUCKYBALL_RUSHB_ACCELERATOR_ID
+#define BUCKYBALL_RUSHB_ACCELERATOR_ID 0
+#endif
+
+#ifndef BUCKYBALL_RUSHB_CHIP_ID
+#define BUCKYBALL_RUSHB_CHIP_ID 0
+#endif
+
 static void __attribute__((constructor)) rushb_runtime_init(void) {
   rushb_init();
+  rushb_select_accelerator(BUCKYBALL_RUSHB_ACCELERATOR_ID,
+                           BUCKYBALL_RUSHB_CHIP_ID);
 }
 
 static void __attribute__((destructor)) rushb_runtime_destroy(void) {
