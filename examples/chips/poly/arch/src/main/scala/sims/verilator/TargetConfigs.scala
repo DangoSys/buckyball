b3e97d259321f8a9ebce444a5e0bed3e90763165
shirohasuki <whmio0115@gmail.com>
2026-09-12 03:10:43 +0800

    [ci] fix: update CI workflows to install configuration and disable unnecessary tests for Goban and Poly chips

--- examples/chips/poly/arch/src/main/scala/sims/verilator/TargetConfigs.scala
+++ examples/chips/poly/arch/src/main/scala/sims/verilator/TargetConfigs.scala
@@ -4,7 +4,7 @@ import org.chipsalliance.cde.config.Config
 
 class BuckyballPolyVerilatorConfig
     extends Config(
-      new BBSimConfig(maxHarts = 5) ++
+      new BBSimConfig(maxHarts = 20) ++
         new WithCustomBootROM ++
         new examples.poly.BuckyballPolyConfig
     )
