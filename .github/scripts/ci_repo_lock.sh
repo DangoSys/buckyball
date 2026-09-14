129ed1e82f7a524c7a5b2ac83ea901ace891b398
daiyongyuan <1533208939@qq.com>
2026-09-14 16:33:25 +0800

    fix: initialize only required nested submodule

--- .github/scripts/ci_repo_lock.sh
+++ .github/scripts/ci_repo_lock.sh
@@ -31,7 +31,8 @@ repo_reset() {
   rm -rf compiler/thirdparty/buddy-mlir/llvm/build
   rm -rf compiler/thirdparty/buddy-mlir/build
   git submodule sync
-  git submodule update --init --force --recursive -- $(git ls-tree -r "${want_sha}" | awk '$1 == "160000" { print $4 }')
+  git submodule update --init --force -- $(git ls-tree -r "${want_sha}" | awk '$1 == "160000" { print $4 }')
+  git submodule update --init --force -- compiler/thirdparty/buddy-mlir/llvm
   for submodule in bbdev bebop compiler/thirdparty/buddy-mlir; do
     git -C "${repo}/${submodule}" reset --hard
     git -C "${repo}/${submodule}" clean -ffd
